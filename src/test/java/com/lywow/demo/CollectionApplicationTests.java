f8794959f9c68eb8240030f0d67a02ba8fda3cf0
lywow <1248040372@qq.com>
2021-04-24 16:45:00 +0800

    first

---
+++ src/test/java/com/lywow/demo/CollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lywow.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
